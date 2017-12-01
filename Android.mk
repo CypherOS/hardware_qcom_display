KineticOS/android_hardware_qcom_display

Revert "msm8974: remove from top level makefile"
This reverts commit 798662177c2d334ce59b232effd3315ee872a33a.

Change-Id: I63514641d6be40bc7d5d96dc3ce44ecfe9567c6a

Android.mk
@@ -4,23 +4,28 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     #This is for 8084 based platforms
     include $(call all-named-subdir-makefiles,msm8084)
   else
-    ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
-      include $(call all-named-subdir-makefiles,msm8226)
+    ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+      #This is for 8974 based (and B-family) platforms
+      include $(call all-named-subdir-makefiles,msm8974)
     else
-      ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-        include $(call all-named-subdir-makefiles,msm8960)
+      ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
+        include $(call all-named-subdir-makefiles,msm8226)
       else
-        ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
-          include $(call all-named-subdir-makefiles,msm8994)
+        ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+          include $(call all-named-subdir-makefiles,msm8960)
         else
-          ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
-            include $(call all-named-subdir-makefiles,msm8909)
+          ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+            include $(call all-named-subdir-makefiles,msm8994)
           else
-            ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
-              include $(call all-named-subdir-makefiles,msm8996)
+            ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
+              include $(call all-named-subdir-makefiles,msm8909)
             else
-              ifneq ($(filter msmcobalt,$(TARGET_BOARD_PLATFORM)),)
-                include $(call all-named-subdir-makefiles,msmcobalt)
+              ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
+                include $(call all-named-subdir-makefiles,msm8996)
+              else
+                ifneq ($(filter msmcobalt,$(TARGET_BOARD_PLATFORM)),)
+                  include $(call all-named-subdir-makefiles,msmcobalt)
+                endif
               endif
             endif
           endif

CypherOS/hardware_qcom_display

Android.mk
@@ -4,23 +4,28 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     #This is for 8084 based platforms
     include $(call all-named-subdir-makefiles,msm8084)
   else
-    ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
-      include $(call all-named-subdir-makefiles,msm8226)
+    ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+      #This is for 8974 based (and B-family) platforms
+      include $(call all-named-subdir-makefiles,msm8974)
     else
-      ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
-        include $(call all-named-subdir-makefiles,msm8960)
+      ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
+        include $(call all-named-subdir-makefiles,msm8226)
       else
-        ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
-          include $(call all-named-subdir-makefiles,msm8994)
+        ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+          include $(call all-named-subdir-makefiles,msm8960)
         else
-          ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
-            include $(call all-named-subdir-makefiles,msm8909)
+          ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+            include $(call all-named-subdir-makefiles,msm8994)
           else
-            ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
-              include $(call all-named-subdir-makefiles,msm8996)
+            ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
+              include $(call all-named-subdir-makefiles,msm8909)
             else
-              ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
-                include $(call all-named-subdir-makefiles,msm8998)
+              ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
+                include $(call all-named-subdir-makefiles,msm8996)
+              else
+                ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
+                  include $(call all-named-subdir-makefiles,msm8998)
+                endif
               endif
             endif
           endif